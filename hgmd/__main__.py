TP calculation bug fixes

hgmd/__main__.py
@@ -63,6 +63,10 @@ def init_parser(parser):
         '-Down', nargs='?',default=False,
         help="Downsample"
     )
+    parser.add_argument(
+        '-Trim', nargs='?',default=2000,
+        help="Trim output files"
+    )
     return parser
 
 
@@ -75,6 +79,7 @@ def read_data(cls_path, tsne_path, marker_path, gene_path, D):
     cls_ser = pd.read_csv(
         cls_path, sep='\t', index_col=0, names=['cell', 'cluster'], squeeze=True
     )
+
     tsne = pd.read_csv(
         tsne_path, sep='\t', index_col=0, names=['cell', 'tSNE_1', 'tSNE_2']
     )
@@ -85,7 +90,7 @@ def read_data(cls_path, tsne_path, marker_path, gene_path, D):
         marker_path,sep='\t', index_col=0
         ).rename_axis('cell',axis=1)
     #gene list filtering
-    #print(no_complement_marker_exp)
+            
     no_complement_marker_exp = np.transpose(no_complement_marker_exp)
 
     #gene filtering
@@ -102,14 +107,25 @@ def read_data(cls_path, tsne_path, marker_path, gene_path, D):
                     pass
                 else:
                     no_complement_marker_exp.drop(column_name, axis=1, inplace=True)
+            
                 
     #-------------#
 
     #downsampling
     #-------------#
+    
     if D is False:
         pass
     else:
+        # get number of genes to set downsample threshold
+        gene_numb = len(no_complement_marker_exp.columns)
+        #print(gene_numb)
+
+        if gene_numb > 300:
+            if int(D) < int(2500):
+                pass
+            else:
+                D = int(2500)
         #total number of cells input
         N = len(cls_ser)
         #print(N)
@@ -160,7 +176,7 @@ def read_data(cls_path, tsne_path, marker_path, gene_path, D):
             
     return (cls_ser, tsne, no_complement_marker_exp, gene_path)
 
-def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_path,pickle_path,cluster_number,K,abbrev):
+def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_path,pickle_path,cluster_number,K,abbrev,cluster_overall,Trim):
     #for cls in clusters:
     # To understand the flow of this section, read the print statements.
     start_cls_time = time.time()
@@ -172,6 +188,13 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
     fc_test = hgmd.batch_fold_change(marker_exp, cls_ser, cls)
     print('Running XL-mHG on singletons...')
     xlmhg = hgmd.batch_xlmhg(marker_exp, cls_ser, cls, X=X, L=L)
+    '''
+    #print(xlmhg.to_string())
+    #tester = marker_exp['Reg4'].copy(deep=True)
+    
+    #tester.sort_values(inplace=True)
+    #print(xlmhg['mHG_cutoff'].to_string())
+    '''
     # We need to slide the cutoff indices before using them,
     # to be sure they can be used in the real world. See hgmd.mhg_slide()
     cutoff_value = hgmd.mhg_cutoff_value(
@@ -204,13 +227,29 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
     ############
     print('Creating discrete expression matrix...')
     discrete_exp = hgmd.discrete_exp(marker_exp, cutoff_value,trips_list)
+    '''
+    #For checking the sliding issue
+    count = 0
+    print(discrete_exp['Reg4'].sort_values(ascending=False).head(667))
+    #time.sleep(100000)
+    print(cls_ser)
+    for index in discrete_exp['Reg4'].sort_values(ascending=False).head(667).iteritems():
+        for index2 in cls_ser.iteritems():
+            if index[0] == index2[0]:
+                if index2[1] == 0:
+                    count = count +1
+    print(count)
+    print(discrete_exp['Reg4'].sort_values(ascending=False).head(70))
+    print(cls_ser.to_string())
+    #print(marker_exp['1600029D21Rik'].sort_values(ascending=False).head(160))
+    #time.sleep(100000)
+    '''
 
     discrete_exp_full = discrete_exp.copy()
     print('Finding simple true positives/negatives for singletons...')
     #Gives us the singleton TP/TNs for COI and for rest of clusters
     #COI is just a DF, rest of clusters are a dict of DFs
-    (sing_tp_tn, other_sing_tp_tn) = hgmd.tp_tn(discrete_exp, cls_ser, cls, cluster_number)
-
+    (sing_tp_tn, other_sing_tp_tn) = hgmd.tp_tn(discrete_exp, cls_ser, cls, cluster_number, cluster_overall)
     '''
     for key in other_sing_tp_tn:
         for index,row in other_sing_tp_tn[key].iterrows():
@@ -232,8 +271,7 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
         gene_map, in_cls_count, pop_count,
         in_cls_product, total_product, upper_tri_indices,
         cluster_exp_matrices, cls_counts
-    ) = hgmd.pair_product(discrete_exp, cls_ser, cls, cluster_number,trips_list)
-
+    ) = hgmd.pair_product(discrete_exp, cls_ser, cls, cluster_number,trips_list,cluster_overall)
 
     if K >= 4:
         print('')
@@ -261,12 +299,10 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
 
     HG_start = time.time()
     print('Running hypergeometric test on pairs...')
-    
     pair, revised_indices = hgmd.pair_hg(
         gene_map, in_cls_count, pop_count,
         in_cls_product, total_product, upper_tri_indices, abbrev
     )
-
     HG_end = time.time()
     print(str(HG_end-HG_start) + ' seconds')
     
@@ -274,10 +310,10 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
     .sort_values(by='HG_stat', ascending=True)
     
     pair_out_initial['rank'] = pair_out_initial.reset_index().index + 1
-    pair_out_initial.to_csv(
-    csv_path + '/cluster_' + str(cls) + '_pair_init_rank.csv'
+    pair_out_print = pair_out_initial.head(Trim)
+    pair_out_print.to_csv(
+    csv_path + '/cluster_' + str(cls) + '_pair_HG_stat_ranked.csv'
     )
-
     if K == 3:
         HG_start = time.time()
         print('Running hypergeometric test & TP/TN on trips...')
@@ -296,7 +332,6 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
             #print(trips)
             HG_end = time.time()
             print(str(HG_end-HG_start) + ' seconds')
-
 
 
     # Pair TP/TN FOR THIS CLUSTER
@@ -322,17 +357,6 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
     sing_tp_tn.set_index(['gene_1'], inplace=True)
     rank_start = time.time()
 
-    '''
-    print(other_pair_tp_tn)
-    for key in other_pair_tp_tn:
-        for index, row in other_pair_tp_tn[key].iterrows():
-            if 'LY6D' in index and 'CD3G_c' in index:
-                print(index)
-                print('cluster ' + str(key))
-                print('TP = ')
-                print(row[0])
-    time.sleep(10000)
-    '''
     print('Finding NEW Rank')
     ranked_pair,histogram = hgmd.ranker(pair,xlmhg,sing_tp_tn,other_sing_tp_tn,other_pair_tp_tn,cls_counts,in_cls_count)
     rank_end = time.time()
@@ -362,13 +386,18 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
     sing_output.drop('finrank',axis=1, inplace=True)
     count = 1
     for index,row in sing_output.iterrows():
+        #print(row[8])
         if count == 100:
             break
         if row[0] >= .05:
             sing_output.at[index,'Plot'] = 0
         else:
-            sing_output.at[index,'Plot'] = 1
+            if row[8] <= .15:
+                sing_output.at[index,'Plot'] = 0
+            else:
+                sing_output.at[index,'Plot'] = 1
             count = count + 1
+
     sing_output.to_csv(
         csv_path + '/cluster_' + str(cls) + '_singleton.csv'
     )
@@ -385,8 +414,9 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
     #    .merge(pair_tp_tn, on=['gene_1', 'gene_2'], how='left')
     #print(pair)
     #pair_output['rank'] = pair_output.reset_index().index + 1
-    ranked_pair.to_csv(
-        csv_path + '/cluster_' + str(cls) + '_pair_ranked.csv'
+    ranked_print = ranked_pair.head(Trim)
+    ranked_print.to_csv(
+        csv_path + '/cluster_' + str(cls) + '_pair_final_ranking.csv'
     )
     #Add trips data pages
     #does not currently do new rank scheme
@@ -395,7 +425,8 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
           .sort_values(by='HG_stat', ascending=True)
           #print(trips_output)
         trips_output['rank'] = trips_output.reset_index().index + 1
-        trips_output.to_csv(
+        trips_print = trips_output.head(Trim)
+        trips_print.to_csv(
             csv_path + '/cluster_' + str(cls) + '_trips.csv'
             )
     else:
@@ -404,7 +435,8 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
         quads_final = quads_fin\
           .sort_values(by='HG_stat', ascending=True)
         quads_final['rank'] = quads_final.reset_index().index + 1
-        quads_final.to_csv(
+        quads_print = quads_final.head(Trim)
+        quads_print.to_csv(
             csv_path + '/cluster_' + str(cls) + '_quads.csv'
             )
     else:
@@ -424,14 +456,14 @@ def process(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_pa
         discrete_exp=discrete_exp_full,
         marker_exp=marker_exp,
         plot_pages=plot_pages,
-        combined_path=vis_path + '/cluster_' + str(cls) + '_combined.pdf',
+        combined_path=vis_path + '/cluster_' + str(cls) + '_pairs_as_singletons',
         sing_combined_path=vis_path + '/cluster_' +
-        str(cls) + '_singleton_combined.pdf',
-        discrete_path=vis_path + '/cluster_' + str(cls) + '_discrete.pdf',
-        tptn_path=vis_path + 'cluster_' + str(cls) + '_TP_TN.pdf',
-        trips_path=vis_path + 'cluster_' + str(cls) + '_discrete_trios.pdf',
-        quads_path=vis_path + 'cluster_' + str(cls) + '_discrete_quads.pdf',
-        sing_tptn_path=vis_path + 'cluster_' + str(cls) + '_singleton_TP_TN.pdf'
+        str(cls) + '_singleton',
+        discrete_path=vis_path + '/cluster_' + str(cls) + '_discrete_pairs',
+        tptn_path=vis_path + 'cluster_' + str(cls) + 'pair_TP_TN',
+        trips_path=vis_path + 'cluster_' + str(cls) + '_discrete_trios',
+        quads_path=vis_path + 'cluster_' + str(cls) + '_discrete_quads',
+        sing_tptn_path=vis_path + 'cluster_' + str(cls) + '_singleton_TP_TN'
         )
     end_cls_time=time.time()
     print(str(end_cls_time - start_cls_time) + ' seconds')
@@ -468,6 +500,7 @@ def main():
     tsne_file = args.tsne
     cluster_file = args.cluster
     gene_file = args.g
+    Trim = args.Trim
     plot_pages = 30  # number of genes to plot (starting with highest ranked)
 
     # TODO: gene pairs with expression ratio within the cluster of interest
@@ -478,9 +511,28 @@ def main():
     csv_path = output_path + 'data/'
     vis_path = output_path + 'vis/'
     pickle_path = output_path + '_pickles/'
-    os.makedirs(csv_path, exist_ok=True)
-    os.makedirs(vis_path, exist_ok=True)
-    os.makedirs(pickle_path, exist_ok=True)
+    try:
+        os.makedirs(csv_path)
+    except:
+        os.system('rm -r ' + csv_path)
+        os.makedirs(csv_path)
+
+    try:
+        os.makedirs(vis_path)
+    except:
+        os.system('rm -r ' + vis_path)
+        os.makedirs(vis_path)
+
+    try:
+        os.makedirs(pickle_path)
+    except:
+        os.system('rm -r ' + pickle_path)
+        os.makedirs(pickle_path)
+
+    if Trim is not None:
+        Trim = int(Trim)
+    else:
+        Trim = int(2000)
     if C is not None:
         C = abs(int(C))
     else:
@@ -517,23 +569,40 @@ def main():
         )
     print("Generating complement data...")
     marker_exp = hgmd.add_complements(no_complement_marker_exp)
+    
     #throw out vals that show up in expression matrix but not in cluster assignments
-    for index,row in marker_exp.iterrows():
-        if index in cls_ser.index.values.tolist():
+    for ind,row in marker_exp.iterrows():
+        if ind in cls_ser.index.values.tolist():
             continue
         else:
-            marker_exp.drop(index, inplace=True)
-    
+            marker_exp.drop(ind, inplace=True)
+        #print(marker_exp.index.values.tolist().count(str(ind)))
+        #print(marker_exp[index])
+    '''
+    #throw out cls_ser vals not in marker_exp
+    for index in cls_ser.index.values.tolist():
+        if index in marker_exp.columns:
+            continue
+        else:
+            cls_ser.drop(index,inplace=True)
+    '''
+
+    marker_exp.sort_values(by='cell',inplace=True)
+    cls_ser.sort_index(inplace=True)
+
+            
     # Process clusters sequentially
     clusters = cls_ser.unique()
     clusters.sort()
-    
+    cluster_overall=clusters.copy()
+    '''
     if clusters[0] == 0:
         cluster_change = np.delete(clusters,0)
         cluster_change_2 = np.append(cluster_change,cluster_change[-1]+1)
         clusters = cluster_change_2
         fin_clus = clusters[-1]
         cls_ser.replace(0,fin_clus,inplace=True)
+    '''
             #print(row)
     #Below could probably be optimized a little (new_clust not necessary),
     #instead of new clust just go from (x-1)n to (x)n in clusters
@@ -546,6 +615,10 @@ def main():
         cores = len(clusters)
     #below loops allow for splitting the job based on core choice
     group_num  = math.ceil((len(clusters) / cores ))
+    #print(group_num)
+    #print(clusters)
+    #print(cluster_overall)
+    #time.sleep(1000)
     for element in range(group_num):
         new_clusters = clusters[:cores]
         print(new_clusters)
@@ -556,14 +629,15 @@ def main():
         #clusters = [1 2 3 4 5 6] & cores = 4 --> new_clusters = [1 2 3 4], new_clusters = [5 6]
         for cls in new_clusters:
             p = multiprocessing.Process(target=process,
-                args=(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_path,pickle_path,cluster_number,K,Abbrev))
+                args=(cls,X,L,plot_pages,cls_ser,tsne,marker_exp,gene_file,csv_path,vis_path,pickle_path,cluster_number,K,Abbrev,cluster_overall,Trim))
             jobs.append(p)
             p.start()
         p.join()
         
+            #print(cls)
         new_clusters = []
         clusters = clusters[cores:len(clusters)]
-        print(clusters)
+        #print(clusters)
 
     end_time = time.time()
 

hgmd/hgmd.py
@@ -46,7 +46,7 @@ def add_complements(marker_exp):
     :rtype: pandas.DataFrame
     """
     for gene in marker_exp.columns:
-        marker_exp[gene + '_c'] = -marker_exp[gene]
+        marker_exp[gene + '_negation'] = -marker_exp[gene]
     '''
     for gene in marker_exp.columns:
         marker_exp[gene + '_c'] = max(marker_exp[gene]) - marker_exp[gene]
@@ -112,6 +112,7 @@ def batch_xlmhg(marker_exp, c_list, coi, X=None, L=None):
         xlmhg.values.tolist(),
         columns=['HG_stat', 'mHG_cutoff', 'mHG_pval']
     )
+    #time.sleep(100000)
     return output
 
 
@@ -227,7 +228,7 @@ def mhg_cutoff_value(marker_exp, cutoff_ind):
         gene = row['gene_1']
         val = marker_exp[gene].sort_values(
             ascending=False).iloc[row['mHG_cutoff']]
-        if re.compile(".*_c$").match(gene):
+        if re.compile(".*_negation$").match(gene):
             return val - FLOAT_PRECISION
         else:
             return val + FLOAT_PRECISION
@@ -321,7 +322,7 @@ def discrete_exp(marker_exp, cutoff_val,trips_list):
     return output
 
 
-def tp_tn(discrete_exp, c_list, coi, cluster_number):
+def tp_tn(discrete_exp, c_list, coi, cluster_number, cluster_overall):
     """Finds simple true positive/true negative values for the cluster of
     interest.
 
@@ -342,46 +343,54 @@ def tp_tn(discrete_exp, c_list, coi, cluster_number):
     #does rest of clusters
     discrete_exp.fillna(0, inplace=True)
     sing_cluster_exp_matrices = {}
-    for clstrs in range(cluster_number):
-        if clstrs+1 == coi:
+    for clstrs in cluster_overall:
+        if clstrs == coi:
             continue
-        mem_list = (c_list == (clstrs+1)) * 1
+        mem_list = (c_list == (clstrs)) * 1
         tp_tn =discrete_exp.apply(
             lambda col: (
                 np.dot(mem_list, col.values) / np.sum(mem_list),
                 np.dot(1 - mem_list, 1 - col.values) / np.sum(1 - mem_list),
             )
         )
-        sing_cluster_exp_matrices[clstrs+1] = pd.DataFrame()
-        sing_cluster_exp_matrices[clstrs+1]['gene_1'] = tp_tn.index
-        sing_cluster_exp_matrices[clstrs+1][['TP', 'TN']] = pd.DataFrame(
+        sing_cluster_exp_matrices[clstrs] = pd.DataFrame()
+        sing_cluster_exp_matrices[clstrs]['gene_1'] = tp_tn.index
+        sing_cluster_exp_matrices[clstrs][['TP', 'TN']] = pd.DataFrame(
             tp_tn.values.tolist(),
             columns=['TP', 'TN']
         )
-        sing_cluster_exp_matrices[clstrs+1].set_index('gene_1',inplace=True)
+        sing_cluster_exp_matrices[clstrs].set_index('gene_1',inplace=True)
         
 
     #does our cluster of interest
     # * 1 converts to integer
     mem_list = (c_list == coi) * 1
+    
     tp_tn = discrete_exp.apply(
         lambda col: (
             np.dot(mem_list, col.values) / np.sum(mem_list),
-            np.dot(1 - mem_list, 1 - col.values) / np.sum(1 - mem_list),
+            np.dot(1 - mem_list, 1 - col.values) / np.sum(1 - mem_list)
         )
     )
+    '''
+    tp_tn = discrete_exp.apply(
+        lambda col: (
+            np.dot(mem_list, col.values) / np.sum(mem_list),
+            np.dot(1 - mem_list, 1 - col.values) / np.sum(1 - mem_list)
+        )
+    )
+    '''
     output = pd.DataFrame()
     output['gene_1'] = tp_tn.index
     output[['TP', 'TN']] = pd.DataFrame(
         tp_tn.values.tolist(),
         columns=['TP', 'TN']
     )
-    
     #outputs a DF for COI and a dict of DF's for rest
     return output, sing_cluster_exp_matrices
 
 
-def pair_product(discrete_exp, c_list, coi, cluster_number, trips_list):
+def pair_product(discrete_exp, c_list, coi, cluster_number, trips_list,cluster_overall):
     """Finds paired expression counts.  Returns in matrix form.
 
     The product of the transpose of the discrete_exp DataFrame is a matrix
@@ -438,20 +447,19 @@ def pair_product(discrete_exp, c_list, coi, cluster_number, trips_list):
     '''
     
     gene_map = discrete_exp.columns
-    #print(discrete_exp)
     in_cls_matrix = discrete_exp[c_list == coi].values
     total_matrix = discrete_exp.values
     #get exp matrices for each non-interest cluster for new rank scheme
     #(clstrs + 1 because range starts a list @ 0)
     cluster_exp_matrices = {}
     cls_counts = {}
-    for clstrs in range(cluster_number):
-        if clstrs+1 == coi:
+    for clstrs in cluster_overall:
+        if clstrs == coi:
             pass
         else:
-            cluster_exp_matrices[clstrs+1] = discrete_exp[c_list == (clstrs+1)].values
-            cls_counts[clstrs+1] = np.size(cluster_exp_matrices[clstrs+1],0)
-            cluster_exp_matrices[clstrs+1]=np.matmul(np.transpose(cluster_exp_matrices[clstrs+1]),cluster_exp_matrices[clstrs+1] )
+            cluster_exp_matrices[clstrs] = discrete_exp[c_list == (clstrs)].values
+            cls_counts[clstrs] = np.size(cluster_exp_matrices[clstrs],0)
+            cluster_exp_matrices[clstrs]=np.matmul(np.transpose(cluster_exp_matrices[clstrs]),cluster_exp_matrices[clstrs] )
 
     in_cls_count = np.size(in_cls_matrix, 0)
     pop_count = np.size(total_matrix, 0)
@@ -890,7 +898,9 @@ def ranker(pair,xlmhg,sing_tp_tn,other_sing_tp_tn,other_pair_tp_tn,cls_counts,in
     def ranked_stat(gene_1,gene_2,lead_gene,cls_counts,other_pair_tp_tn,other_sing_tp_tn,in_cls_count):
         stats=[]
         stats_debug = {}
+        #print(cls_counts)
         for clstrs in cls_counts:
+            #print(clstrs)
             #this small cluster ignoring doesnt really matter when using
             #the sum and not the max
             #if cls_counts[clstrs] <= ( .05 * in_cls_count):
@@ -913,7 +923,7 @@ def ranker(pair,xlmhg,sing_tp_tn,other_sing_tp_tn,other_pair_tp_tn,cls_counts,in
     xlmhg_cop = xlmhg_cop.set_index('gene_1')
     ranked_pair = pair.copy()
     ranked_pair.sort_values(by='HG_stat',ascending=True,inplace=True)
-    ranked_pair['initrank'] = ranked_pair.reset_index().index + 1
+    ranked_pair['HG_rank'] = ranked_pair.reset_index().index + 1
     #print(ranked_pair)
     
     #below not used because this does ALL pairs (too many)
@@ -998,10 +1008,10 @@ def ranker(pair,xlmhg,sing_tp_tn,other_sing_tp_tn,other_pair_tp_tn,cls_counts,in
     loopend = time.time()
     ranked_pair.sort_values(by='CCS',ascending=False,inplace=True)
     #print(ranked_pair)
-    ranked_pair['CCSrank'] = ranked_pair.reset_index().index + 1
+    ranked_pair['CCS_rank'] = ranked_pair.reset_index().index + 1
     #print(ranked_pair)
     #time.sleep(1000)
-    ranked_pair['finalrank'] = ranked_pair[['initrank', 'CCSrank']].mean(axis=1)
+    ranked_pair['finalrank'] = ranked_pair[['HG_rank', 'CCS_rank']].mean(axis=1)
     #print(ranked_pair)
     ranked_pair.sort_values(by='finalrank',ascending=True,inplace=True)
     #print(ranked_pair)
@@ -1017,6 +1027,7 @@ def ranker(pair,xlmhg,sing_tp_tn,other_sing_tp_tn,other_pair_tp_tn,cls_counts,in
     else:
         plot_num = 100
     for index,row in ranked_pair.iterrows():
+        #print(row[3])
         if count == plot_num:
             break
         if row[0] in omit_genes:
@@ -1026,7 +1037,10 @@ def ranker(pair,xlmhg,sing_tp_tn,other_sing_tp_tn,other_pair_tp_tn,cls_counts,in
                 if row[2] >= .05:
                     ranked_pair.at[index,'Plot'] = 0
                 else:
-                    ranked_pair.at[index,'Plot'] = 1
+                    if row[3] <= .15:
+                        ranked_pair.at[index,'Plot'] = 0
+                    else:
+                        ranked_pair.at[index,'Plot'] = 1
                 omit_genes[row[0]] = omit_genes[row[0]]+1
                 count = count + 1
         else:
@@ -1074,8 +1088,6 @@ def pair_tp_tn(gene_map, in_cls_count, pop_count, in_cls_product,
     """
 
 
-
-    
     def tp(taken_in_cls):
         return taken_in_cls / in_cls_count
 
@@ -1102,13 +1114,12 @@ def pair_tp_tn(gene_map, in_cls_count, pop_count, in_cls_product,
 
         return output
         
-    else:   
+    else:
+
         tp_result = np.vectorize(tp)(in_cls_product[upper_tri_indices])
         tn_result = np.vectorize(tn)(
             in_cls_product[upper_tri_indices], total_product[upper_tri_indices]
             )
-
-    
         output = pd.DataFrame({
             'gene_1': gene_map[upper_tri_indices[0]],
             'gene_2': gene_map[upper_tri_indices[1]],

hgmd/visualize.py
@@ -5,6 +5,8 @@ from matplotlib import cm
 from matplotlib.backends.backend_pdf import PdfPages
 from itertools import repeat
 import time
+import os
+from adjustText import adjust_text
 
 """
 Set of modularized visualization functions for COMET; producing graphs in PDFs.
@@ -64,7 +66,6 @@ def make_plots(
         pass
     else:
         q_short=quads_fin.iloc[:plot_pages]
-
     vmt = np.vectorize(make_title)
     d_plot_genes = zip(
         zip(
@@ -85,7 +86,6 @@ def make_plots(
             )
         ), p_short['gene_1'].values, p_short['gene_2'].values
     )
-
     if type(trips) == int:
         pass
     else:
@@ -143,9 +143,11 @@ def make_plots(
 
     
     print("Drawing discrete plots for pairs...")
+    
     make_discrete_plots(
         tsne, discrete_exp, d_plot_genes, discrete_path, 2
     )
+    
     if type(trips) == int:
         pass
     else:
@@ -180,9 +182,11 @@ def make_plots(
         ), p_short['gene_1'].values, p_short['gene_2'].values
     )
     print("Drawing combined plots...")
+    
     make_combined_plots(
         tsne, discrete_exp, marker_exp, c_plot_genes, combined_path
     )
+    
     c_s_plot_genes = zip(
         zip(
             vmt(
@@ -193,19 +197,21 @@ def make_plots(
         ), s_short.index, repeat(np.nan)
     )
     print("Drawing singleton combined plots...")
+    
     make_combined_plots(
         tsne, discrete_exp, marker_exp, c_s_plot_genes, sing_combined_path
     )
+    
     pair_tp_tn = pair[['gene_1', 'gene_2', 'TP', 'TN']]
     sing_tp_tn = sing[['TP', 'TN']]
     print("Drawing true positive/negative plots...")
     make_tp_tn_plot(
         zip(p_short['gene_1'], p_short['gene_2']),
-        sing_tp_tn, pair_tp_tn, tptn_path
+        sing_tp_tn, pair_tp_tn, tptn_path, 0
     )
     make_tp_tn_plot(
         zip(s_short.index, repeat(np.nan)),
-        sing_tp_tn, pair_tp_tn, sing_tptn_path
+        sing_tp_tn, pair_tp_tn, sing_tptn_path, 1
     )
 
 
@@ -358,82 +364,90 @@ def make_discrete_plots(tsne, discrete_exp, plot_genes, path,num):
             cmap=CMAP_DISCRETE
         )
 
-    with PdfPages(path) as pdf:
-        for plot_gene in plot_genes:
-            # print(plot_gene)
-            if num == 4:
-                fig, ax_triple = plt.subplots(ncols=5, figsize=(15, 5))
-                if pd.isnull(plot_gene[2]):
-                    make_single_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        title=plot_gene[0],
-                        gene=plot_gene[1]
-                        )
-                elif pd.isnull(plot_gene[3]):
-                    make_pair_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        titles=plot_gene[0],
-                        gene_1=plot_gene[1],
-                        gene_2=plot_gene[2]
-                        )
-                elif pd.isnull(plot_gene[4]):
-                    make_trips_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        titles=plot_gene[0],
-                        gene_1=plot_gene[1],
-                        gene_2=plot_gene[2],
-                        gene_3=plot_gene[3]
-                        )
-                else:
-                    make_quads_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        titles=plot_gene[0],
-                        gene_1=plot_gene[1],
-                        gene_2=plot_gene[2],
-                        gene_3=plot_gene[3],
-                        gene_4=plot_gene[4]
-                        )
-            elif num == 3:
-                fig, ax_triple = plt.subplots(ncols=4, figsize=(15, 5))
-                if pd.isnull(plot_gene[2]):
-                    make_single_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        title=plot_gene[0],
-                        gene=plot_gene[1]
-                        )
-                elif pd.isnull(plot_gene[3]):
-                    make_pair_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        titles=plot_gene[0],
-                        gene_1=plot_gene[1],
-                        gene_2=plot_gene[2]
-                        )
-                else:
-                    make_trips_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        titles=plot_gene[0],
-                        gene_1=plot_gene[1],
-                        gene_2=plot_gene[2],
-                        gene_3=plot_gene[3]
-                        )
+    #with PdfPages(path) as pdf:
+    count = 1
+    try:
+        os.makedirs(path)
+    except:
+        os.system('rm -r ' + path)
+        os.makedirs(path)
+    for plot_gene in plot_genes:
+        # print(plot_gene)
+        if num == 4:
+            fig, ax_triple = plt.subplots(ncols=5, figsize=(15, 5))
+            if pd.isnull(plot_gene[2]):
+                make_single_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    title=plot_gene[0],
+                    gene=plot_gene[1]
+                    )
+            elif pd.isnull(plot_gene[3]):
+                make_pair_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    titles=plot_gene[0],
+                    gene_1=plot_gene[1],
+                    gene_2=plot_gene[2]
+                    )
+            elif pd.isnull(plot_gene[4]):
+                make_trips_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    titles=plot_gene[0],
+                    gene_1=plot_gene[1],
+                    gene_2=plot_gene[2],
+                    gene_3=plot_gene[3]
+                    )
             else:
-                fig, ax_triple = plt.subplots(ncols=3, figsize=(15, 5))
-                if pd.isnull(plot_gene[2]):
-                    make_single_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        title=plot_gene[0],
-                        gene=plot_gene[1]
-                        )
-                else:
-                    make_pair_discrete_page(
-                        fig=fig, ax_triple=ax_triple,
-                        titles=plot_gene[0],
-                        gene_1=plot_gene[1],
-                        gene_2=plot_gene[2]
-                        )
+                make_quads_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    titles=plot_gene[0],
+                    gene_1=plot_gene[1],
+                    gene_2=plot_gene[2],
+                    gene_3=plot_gene[3],
+                    gene_4=plot_gene[4]
+                    )
+        elif num == 3:
+            fig, ax_triple = plt.subplots(ncols=4, figsize=(15, 5))
+            if pd.isnull(plot_gene[2]):
+                make_single_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    title=plot_gene[0],
+                    gene=plot_gene[1]
+                    )
+            elif pd.isnull(plot_gene[3]):
+                make_pair_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    titles=plot_gene[0],
+                    gene_1=plot_gene[1],
+                    gene_2=plot_gene[2]
+                    )
+            else:
+                make_trips_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    titles=plot_gene[0],
+                    gene_1=plot_gene[1],
+                    gene_2=plot_gene[2],
+                    gene_3=plot_gene[3]
+                    )
+        else:
+            fig, ax_triple = plt.subplots(ncols=3, figsize=(15, 5))
+            if pd.isnull(plot_gene[2]):
+                make_single_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    title=plot_gene[0],
+                    gene=plot_gene[1]
+                    )
+            else:
+                make_pair_discrete_page(
+                    fig=fig, ax_triple=ax_triple,
+                    titles=plot_gene[0],
+                    gene_1=plot_gene[1],
+                    gene_2=plot_gene[2]
+                    )
                 
-            pdf.savefig(fig)
-            plt.close(fig)
+            #pdf.savefig(fig)
+        plt.savefig( path + '/' + 'rank_' + str(count) + '.png')
+        count=count+1
+        plt.close(fig)
 
 def make_combined_plots(tsne, discrete_exp, marker_exp, plot_genes, path):
     """Plots discrete alongside continuous expression to PDF.
@@ -504,25 +518,33 @@ def make_combined_plots(tsne, discrete_exp, marker_exp, plot_genes, path):
             cmap=CMAP_CONTINUOUS, draw_cbar=True
         )
 
-    with PdfPages(path) as pdf:
-        for plot_gene in plot_genes:
-            if pd.isnull(plot_gene[2]):
-                fig, axes = plt.subplots(ncols=2, figsize=(10, 5))
-                make_single_combined_page(
-                    fig, plot_gene[0][0], axes, plot_gene[1]
-                )
-            else:
-                fig, axes = plt.subplots(
-                    nrows=2, ncols=2, figsize=(10, 10)
-                )
-                make_pair_combined_page(
-                    fig, axes, plot_gene[0], plot_gene[1], plot_gene[2]
-                )
-            pdf.savefig(fig)
-            plt.close(fig)
+    #with PdfPages(path) as pdf:
+    counter = 1
+    try:
+        os.makedirs(path)
+    except:
+        os.system('rm -r ' + path)
+        os.makedirs(path)
+    for plot_gene in plot_genes:
+        if pd.isnull(plot_gene[2]):
+            fig, axes = plt.subplots(ncols=2, figsize=(10, 5))
+            make_single_combined_page(
+                fig, plot_gene[0][0], axes, plot_gene[1]
+            )
+        else:
+            fig, axes = plt.subplots(
+                nrows=2, ncols=2, figsize=(10, 10)
+            )
+            make_pair_combined_page(
+                fig, axes, plot_gene[0], plot_gene[1], plot_gene[2]
+            )
+        #pdf.savefig(fig)
+        plt.savefig( path + '/' + 'rank_' + str(counter) + '.png')
+        counter=counter+1
+        plt.close(fig)
 
 
-def make_tp_tn_plot(plot_genes, sing_tp_tn, pair_tp_tn, path):
+def make_tp_tn_plot(plot_genes, sing_tp_tn, pair_tp_tn, path,sing_pair):
     """Plots TP/TN rates of genes/pairs to PDF.
 
     For each gene/gene pair listed in plot_genes, plot their TP/TN rate on a
@@ -567,9 +589,43 @@ def make_tp_tn_plot(plot_genes, sing_tp_tn, pair_tp_tn, path):
         data, columns=['title', 'TP', 'TN']
     )
 
-    plt.scatter(coords_df['TP'], coords_df['TN'], s=3)
-    for index, row in coords_df.iterrows():
-        plt.annotate(row['title'], (row['TP'] + PADDING, row['TN'] + PADDING))
+    if sing_pair == 1:
+        new_list = [['zero',0,0]]
+        for index, row in coords_df.iterrows():
+            new_list.append([row['title'],row['TP'],row['TN']])
+        new_df = pd.DataFrame(new_list, columns = ['title','TP','TN'])    
+        #print(new_df)
+        concat_new_df = new_df.head(50)
+        #concat_new_df.drop(0,inplace=True)
+        plt.scatter(concat_new_df['TP'], concat_new_df['TN'], s=3,c='red')
+        #print(coords_df)
+        #time.sleep(10000)
+        texts = []
+        for x,y,s in zip(concat_new_df['TP'],concat_new_df['TN'],concat_new_df['title']):
+            texts.append(plt.text(x, y, s, size=7))
+        adjust_text(texts,arrowprops=dict(arrowstyle="->", color='blue', lw=0.5))
 
-    fig.savefig(path)
+
+        
+    else:
+        new_list = [['zero',0,0]]
+        for index, row in coords_df.iterrows():
+            TP = row['TP'].tolist()[-1]
+            TN = row['TN'].tolist()[-1]
+            new_list.append([row['title'],TP,TN])
+        new_df = pd.DataFrame(new_list, columns = ['title','TP','TN'])    
+        #print(new_df)
+        concat_new_df = new_df.head(50)
+        #concat_new_df.drop(0,inplace=True)
+        plt.scatter(concat_new_df['TP'], concat_new_df['TN'], s=3,c='red')
+        #print(coords_df)
+        #time.sleep(10000)
+        texts = []
+        for x,y,s in zip(concat_new_df['TP'],concat_new_df['TN'],concat_new_df['title']):
+            texts.append(plt.text(x, y, s, size=7))
+        adjust_text(texts,arrowprops=dict(arrowstyle="->", color='blue', lw=0.5))
+        
+    #for index, row in coords_df.iterrows():
+    #    plt.annotate(row['title'], (row['TP'] + PADDING, row['TN'] + PADDING))
+    fig.savefig(path + '.png')
     plt.close(fig)